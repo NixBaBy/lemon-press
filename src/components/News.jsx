d6ecfbf597b071d54ec019c140fbc44dadc83dc8
NixBaBy <115783986+NixBaBy@users.noreply.github.com>
2024-12-17 19:05:15 +0800

    asuuh yum beldew

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,34 @@
 import "../style/news.css";
+const news = [
+  {
+    img: "https://dash-api.yld.mn/api/image/58348918-6f3f-48bb-a049-8e8b2f71237e",
+    tittle: "Зах зээл",
+    h2: "Орон сууцны үнэ иргэдийн орлогоос  2-2.5 дахин үнэтэй байна",
+    name: "С.Ангирмаа/",
+    date: "12 сарын 17, 2024",
+  },
+  {
+    img: "https://dash-api.yld.mn/api/image/2d83d835-062d-4a85-a4c0-c7b3887f278d",
+    tittle: "Эдийн засаг",
+    h2: "Казакстаны хөгжлийн цаана",
+    name: "М.Хулан /",
+    date: "12 сарын 17, 2024",
+  },
+  {
+    img: "https://dash-api.yld.mn/api/image/c00b3bc3-2340-49f4-84d3-a274bd10893f",
+    tittle: "Сурталчилгаа",
+    h2: "Ариглан гамнаж, ашигтай хадгал",
+    name: "Б. Эмүжин /",
+    date: "12 сарын 17, 2024",
+  },
+  {
+    img: "https://dash-api.yld.mn/api/image/466e9012-492b-43c9-9449-aa118031efa9",
+    tittle: "Эдийн засаг",
+    h2: "Уул уурхайн салбар гадаадын хөрөнгө оруулалтын 80% гаруйг бүрдүүлж байна",
+    name: "М.Хулан /",
+    date: "12 сарын 16, 2024",
+  },
+];
 
 const News = (props) => {
   return (
@@ -17,4 +47,22 @@ const News = (props) => {
     </div>
   );
 };
+const Neww = () => {
+  return (
+    <div className="new">
+      {news.map((medee) => {
+        return (
+          <News
+            img={medee.img}
+            h2={medee.h2}
+            tittle={medee.tittle}
+            name={medee.name}
+            date={medee.date}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
 export default News;
